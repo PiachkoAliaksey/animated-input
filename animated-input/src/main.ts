901af5045cf4f6c81b445e5f9140422c612d3ce8
PiachkoAliaksey <alek17y@mail.ru>
2023-09-26 22:11:33 +0300

    refactor: change tree structure of folder ans links

---
+++ animated-input/src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+import { renderContent } from './RenderContent/RenderContent';
+import { mainComponent } from './MainComponent/MainComponent';
+
+renderContent()
+
+mainComponent();
+
